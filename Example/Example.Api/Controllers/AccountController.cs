0ebf920835d7418fae0acb7d5127b6a95ad828b5
ozan <ozanakaydin@gmail.com>
2024-06-15 12:26:24 +0300

    Example api updated

--- Example/Example.Api/Controllers/AccountController.cs
+++ Example/Example.Api/Controllers/AccountController.cs
@@ -12,11 +12,13 @@ public class AccountController : ControllerBase
 {
     private readonly MongoUserManager<AppUser> _userManager;
     private readonly MongoRoleManager<MongoIdentityRole> _roleManager;
+    private readonly MongoUserRoleManager<AppUser, MongoIdentityRole> _userRoleManager;
 
-    public AccountController(MongoUserManager<AppUser> userManager,MongoRoleManager<MongoIdentityRole>roleManager)
+    public AccountController(MongoUserManager<AppUser> userManager, MongoRoleManager<MongoIdentityRole> roleManager, MongoUserRoleManager<AppUser, MongoIdentityRole> userRoleManager)
     {
         _userManager = userManager;
         _roleManager = roleManager;
+        _userRoleManager = userRoleManager;
     }
 
     [HttpPost]
@@ -41,15 +43,106 @@ public class AccountController : ControllerBase
         return Ok(result);
     }
 
+    [HttpGet]
+    public async Task<IActionResult> GetRoles(CancellationToken cancellationToken)
+    {
+        var result = await _roleManager.GetRolesAsync(cancellationToken);
+        return Ok(result);
+    }
+
+
+    [HttpGet]
+    public async Task<IActionResult> GetRole(string roleName, CancellationToken cancellationToken)
+    {
+        var result = await _roleManager.GetRoleByNameAsync(roleName, cancellationToken);
+        return Ok(result);
+    }
+
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetRoleById(string id, CancellationToken cancellationToken)
+    {
+        var result = await _roleManager.GetRoleByIdAsync(id, cancellationToken);
+        return Ok(result);
+    }
+
+
     [HttpPost]
-    public async Task<IActionResult> CreateRole([FromBody]CreateRoleDto createRole,CancellationToken cancellationToken)
+    public async Task<IActionResult> CreateRole([FromBody] CreateRoleDto createRole, CancellationToken cancellationToken)
     {
         var result = await _roleManager.CreateAsync(new MongoIdentityRole
         {
             RoleName = createRole.RoleName,
             Description = createRole.Description
 
-        },cancellationToken);
+        }, cancellationToken);
         return Ok(result);
     }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateRole([FromBody] UpdateRoleDto updateRole, CancellationToken cancellationToken)
+    {
+        var result = await _roleManager.UpdateAsync(new MongoIdentityRole
+        {
+            Id = updateRole.Id,
+            RoleName = updateRole.RoleName,
+            Description = updateRole.Description
+        }, cancellationToken);
+        return Ok(result);
+    }
+
+    [HttpDelete("{roleName}")]
+    public async Task<IActionResult> DeleteRole(string roleName, CancellationToken cancellationToken)
+    {
+        var result = await _roleManager.DeleteAsync(roleName, cancellationToken);
+
+        if (result.IsSuccess)
+            return Ok(result.Message);
+
+        return BadRequest(result.Message);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AssignRole(string email, string roleName, CancellationToken cancellationToken)
+    {
+        var userResult = await _userManager.GetUserAsync(email, cancellationToken);
+        if(!userResult.IsSuccess)
+        {
+            return BadRequest(userResult.Message);
+        }
+
+        var result = await _userRoleManager.AddRoleToUserAsync(userResult.Value!, roleName, cancellationToken);
+
+        if (result.IsSuccess)
+            return Ok(result.Message);
+
+        return BadRequest(result.Message);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult>RemoveRoleFromUser(string email, string roleName, CancellationToken cancellationToken)
+    {
+        var userResult = await _userManager.GetUserAsync(email, cancellationToken);
+        if (!userResult.IsSuccess)
+        {
+            return BadRequest(userResult.Message);
+        }
+
+        var result = await _userRoleManager.RemoveRoleFromUserAsync(userResult.Value!, roleName, cancellationToken);
+
+        if (result.IsSuccess)
+            return Ok(result.Message);
+
+        return BadRequest(result.Message);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetUsersInRole(string roleName, CancellationToken cancellationToken)
+    {
+        var result = await _userRoleManager.GetUsersInRoleAsync(roleName, cancellationToken);
+        if (!result.IsSuccess)
+            return BadRequest(result.Message);
+        return Ok(result);
+    }
+
 }
---
+++ Example/Example.Api/Dtos/UpdateRoleDto.cs
@@ -0,0 +1,4 @@
+﻿namespace Example.Api.Dtos;
+
+public record UpdateRoleDto(string Id, string? RoleName, string? Description);
+
